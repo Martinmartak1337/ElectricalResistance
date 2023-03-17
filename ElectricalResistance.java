9d12eb88d9d111696daff860646fa3026174f6ba
Martinmartak1337 <75758821+Martinmartak1337@users.noreply.github.com>
2023-03-17 16:02:50 +0100

    Update ElectricalResistance.java

--- ElectricalResistance.java
+++ ElectricalResistance.java
@@ -1,26 +1,23 @@
 public class ElectricalResistance {
     public static void main(String[] args){
-        System.out.println(parallelElectricalResistance()); // <- Your values
-        System.out.println(serialElectricalResistance()); // <- Your values
+        System.out.println(parallelElectricalResistance(1,2)); // <- Your doubles
+        System.out.println(serialElectricalResistance(8,5,9)); // <- Your doubles
     }
     public static double parallelElectricalResistance(double... Resistance){
-        int x = 0;
         double resistance=0;
         if(Resistance.length == 2){
             return (Resistance[0]*Resistance[1])/(Resistance[0]+Resistance[1]);
         } else {
             for(double i : Resistance){
-                resistance = 1/Resistance[x]+resistance;
-                x++;
+                resistance = 1/Resistance[(int) i]+resistance;
             }
             return 1/resistance;
         }
     }
     public static double serialElectricalResistance(double... resistance){
-        int x = 0;
         double Resistance=0;
         for(double b : resistance){
-            Resistance = Resistance + resistance[x];
+            Resistance = Resistance + resistance[(int) b];
         }
         return Resistance;
     }
