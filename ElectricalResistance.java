01beff68d34a13c10a15748258d3876d14c2f2f9
Martinmartak1337 <75758821+Martinmartak1337@users.noreply.github.com>
2023-04-09 21:45:50 +0200

    Add files via upload

---
+++ ElectricalResistance.java
@@ -0,0 +1,25 @@
+public class ElectricalResistance {
+    public static void main(String[] args){
+        System.out.println(parallelElectricalResistance(1,2)); // <- Your doubles
+        System.out.println(serialElectricalResistance(8,5,9)); // <- Your doubles
+    }
+    public static double parallelElectricalResistance(double... Resistance){
+        double resistance=0;
+        if(Resistance.length == 2){
+            return (Resistance[0]*Resistance[1])/(Resistance[0]+Resistance[1]);
+        } else {
+            for(double i : Resistance){
+                resistance = 1/Resistance[(int) i]+resistance;
+            }
+            return resistance/1;
+        }
+    }
+    public static double serialElectricalResistance(double... resistance){
+        double Resistance=0;
+
+        for (int i = 0; i < resistance.length-1; i++) {
+            Resistance = Resistance + resistance[i];
+        }
+        return Resistance;
+    }
+}
